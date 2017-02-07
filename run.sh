8528c6b17b9b292fd0766a22bb38666677035aee
liukai <659801@qq.com>
2017-02-07 13:33:18 +0800

    修复启动脚本

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sudo docker rm -f reverse-cache
 sudo docker run -d --name reverse-cache \
                 -v $PWD/cache:/node/cache miniers/reverse-proxy-cache \
-                -p 1314:80
+                -p 1314:80 \
+                miniers/reverse-proxy-cache
